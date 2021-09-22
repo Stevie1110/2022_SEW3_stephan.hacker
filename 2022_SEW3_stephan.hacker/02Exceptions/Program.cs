93b09b68d4f7562ea8bb303201dbf001b7bddd05
Stevie1110 <stephan.hacker@josephinum.at>
2021-09-22 17:41:02 +0200

    Projekt Exceptions begonnen

---
+++ 2022_SEW3_stephan.hacker/02Exceptions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace _02Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = File.ReadAllText(@"C:\DEV\myFile.txt");        // @ sorgt dafür, dass \ nicht als Formatierungszeichen gesehen wird.
+                Console.WriteLine("Der Text in der Datei lautet:");
+                Console.WriteLine(text);
+            }catch(FileNotFoundException)
+            {
+                //hier gehts weiter nächste stunde
+            }
+        }
+    }
+}
