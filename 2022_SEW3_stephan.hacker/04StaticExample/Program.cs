6216deeadde6f1f7308fc881dc794cadaee962eb
Stevie1110 <stephan.hacker@josephinum.at>
2021-10-06 16:50:11 +0200

    Statische Methode gemacht

---
+++ 2022_SEW3_stephan.hacker/04StaticExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04StaticExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example ex = new Example();
+            ex.doSomething();
+
+            Example.doSomethingElse();              //Statische Methode
+        }
+    }
+}
