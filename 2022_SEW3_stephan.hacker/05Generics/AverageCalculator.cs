cd1feeabcc864d9c8701961b49d452318202d63e
Stevie1110 <stephan.hacker@josephinum.at>
2021-11-03 15:00:48 +0100

    Dictionaries fertig

---
+++ 2022_SEW3_stephan.hacker/05Generics/AverageCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05Generics
+{
+    class AverageCalculator
+    {
+        public static double AverageOfInput()
+        {
+            Console.WriteLine("Ich berechne den Mittelwert der eingegebenen Zahlen. Beliebiges Zeichen für Ende der Eingabe!");
+            double inputValue;
+
+            List<double> values = new List<double>();
+
+            while (true)
+            {
+                //break; // unterbricht eine schleife
+                Console.Write("Bitte ine Zahl eingeben");
+
+                try
+                {
+                    inputValue = double.Parse(Console.ReadLine());
+                    values.Add(inputValue); //.Add fügt ein Element am ende der Liste hinzu
+                }
+                catch (Exception)
+                {
+                    //nichts tun weil wir die Eingabe beenden wollen
+
+                    double sum = 0.0;
+
+                    foreach (double element in values)
+                    {
+                        sum += element;
+                    }
+
+                    if (values.Count > 0)
+                    {
+                        return sum / values.Count;
+                    }
+                    else
+                    {
+                        return 0.0;
+                    }
+                }
+            }
+        }
+    }
+}
