1c81562d22f5a76ac9bd033f48e3a316983adfb7
Stevie1110 <stephan.hacker@josephinum.at>
2021-11-10 16:46:17 +0100

    Caesar fertig

---
+++ 2022_SEW3_stephan.hacker/08Caeser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08Caeser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erstelle eine Klasse Caesar, in dieser Klasse eine statische Methode
+
+            //public string generateChiffre(string text, int key)
+
+
+
+            string text = ("Ich bin einer kurzer aber geheimer Text");
+
+            string chiffre = Caesar.generateChiffre(text, 300);
+
+            Console.WriteLine(chiffre);
+
+            text = Caesar.generateText(chiffre, 300);
+
+            Console.WriteLine(text);
+        }
+    }
+}
