50aaf52b954438fb1fa4490c2ebd95518b779a69
Stevie1110 <stephan.hacker@josephinum.at>
2021-11-17 15:01:12 +0100

    10 fertig

---
+++ 2022_SEW3_stephan.hacker/09InversionChiffre/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09InversionChiffre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementieren sie folgende Verschlüsselung
+            //z.B.:
+            //Machen Sie es gut
+            //nehcaM eiS se tug!
+
+            Console.Write("Bitte einen Text eingeben!");
+            string text = Console.ReadLine();
+            string result = InversionChiffre.generateChiffre(text);
+            Console.WriteLine(result);
+        }
+    }
+}
