197dff028a6b98a8836fbcaf9ca59b76dee07c37
Stevie1110 <stephan.hacker@josephinum.at>
2021-12-15 14:34:02 +0100

    Test

---
+++ 2022_SEW3_stephan.hacker/2.1_Vorkommen/AnalyseText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._1_Vorkommen
+{
+    class AnalyseText
+    {
+        public static void analyse(string text, string suchbegriff)
+        {
+            string[] textAr = text.Split(suchbegriff);
+            Console.WriteLine(textAr.Length - 1);
+        }
+    }
+}
---
+++ 2022_SEW3_stephan.hacker/2.2_Häufigkeitsanalyse/WordCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._2_Häufigkeitsanalyse
+{
+    class WordCount
+    {
+        Dictionary<string, int> myWords = new Dictionary<string, int>();
+
+        public void MyWords(Dictionary<string, int> myWords)
+        {
+            this.myWords = myWords;
+        }
+
+        public void frequencyAnalysis(string text)
+        {
+            foreach (KeyValuePair<string, int> item in myWords)
+            {
+                string[] textAr = text.Split(item.Key);
+                myWords[item.Key] = textAr.Length - 1;
+            }
+        }
+
+        public void frequencyAnalysisOfFile(string pathToFile)
+        {
+            string text = System.IO.File.ReadAllText(pathToFile);
+            foreach (KeyValuePair<string, int> item in myWords)
+            {
+                string[] textAr = text.Split(item.Key);
+                myWords[item.Key] = textAr.Length - 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            string text = "Heufgkeitsanalyse:\n";
+            foreach (KeyValuePair<string, int> item in myWords)
+            {
+                text = text + item.Key + " - " + item.Value + "\n";
+            }
+        }
+    }
+}
