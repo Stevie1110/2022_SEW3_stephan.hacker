eb40c7ee4f9fe4344dec39da75f262495fe88330
Stevie1110 <stephan.hacker@josephinum.at>
2022-03-16 16:51:28 +0100

    23 fast fertig

---
+++ 2022_SEW3_stephan.hacker/22SerialArduino/ArduinoSerialPortCommunication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace _22SerialArduino
+{
+    public class ArduinoSerialPortCommunication
+    {
+        private SerialPort serialPort;
+
+        public ArduinoSerialPortCommunication(string port)
+        {
+            serialPort = new SerialPort(port, 9600, Parity.None, 8, StopBits.One);
+        }
+
+        public void write(string value)
+        {
+            try
+            {
+                serialPort.Open();
+                serialPort.Write(value);
+            }
+            catch
+            {
+                //aktuell noch keine Fehlerbehandlung
+            }
+            finally
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
