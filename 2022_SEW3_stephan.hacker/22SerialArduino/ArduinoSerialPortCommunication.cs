3c48ffad9a5e5f18af81cacf96c4a5d77ac05384
Stevie1110 <stephan.hacker@josephinum.at>
2022-03-23 15:05:29 +0100

    24 fertig

--- 2022_SEW3_stephan.hacker/22SerialArduino/ArduinoSerialPortCommunication.cs
+++ 2022_SEW3_stephan.hacker/22SerialArduino/ArduinoSerialPortCommunication.cs
@@ -10,13 +10,31 @@ namespace _22SerialArduino
         public ArduinoSerialPortCommunication(string port)
         {
             serialPort = new SerialPort(port, 9600, Parity.None, 8, StopBits.One);
+            serialPort.Open();      // verbindung aufbauen, um auf Nachrichten zu hören. bleibt solange offen, wie das Programm läuft
+
+            //serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);     //ein neuer EventHandler, der eine Methode aufruft
+                                                                                                           //wenn Daten empfangen wurden
+
+            //serialPort.DataReceived += SerialPort_DataReceived;        // diese Zeile hat gleiche Bedeutung wie Zeile 15                                  
+
         }
+
+        public void addSerialDataReceivedEventHandler(SerialDataReceivedEventHandler handler)
+        {
+            serialPort.DataReceived += handler;     //ein neuer EventHandler, der eine Methode aufruft
+                                                    //wenn Daten empfangen wurden
+                                                   
+
+        //private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    Console.Write(serialPort.ReadExisting());
+        //}
 
         public void write(string value)
         {
             try
             {
-                serialPort.Open();
+               
                 serialPort.Write(value);
             }
             catch
@@ -25,7 +43,7 @@ namespace _22SerialArduino
             }
             finally
             {
-                serialPort.Close();
+               
             }
         }
     }
--- 2022_SEW3_stephan.hacker/23SerialArduinoConsole/Program.cs
+++ 2022_SEW3_stephan.hacker/23SerialArduinoConsole/Program.cs
@@ -8,14 +8,18 @@ namespace _23SerialArduinoConsole
         static void Main(string[] args)
         {
             ArduinoSerialPortCommunication arduino1 = new ArduinoSerialPortCommunication("COM4");
-            Console.Write("Bitte 'e' oder 'a' eingeben, um LED ein- oder auszuschalten: ");
-            string input = Console.ReadLine();
-            if(input == "e" || input == "a")
+            while (true)
             {
-                arduino1.write(input);
-            }else
-            {
-                Console.WriteLine("Ungültiger Befehl. Nur 'e' und 'a' erlaubt");
+                Console.Write("Bitte 'e' oder 'a' eingeben, um LED ein- oder auszuschalten: ");
+                string input = Console.ReadLine();
+                if (input == "e" || input == "a")
+                {
+                    arduino1.write(input);
+                }
+                else
+                {
+                    Console.WriteLine("Ungültiger Befehl. Nur 'e' und 'a' erlaubt");
+                }
             }
         }
     }
