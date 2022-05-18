a8764967d5bbbefb94eb76f42d93251645f2d0b2
Stevie1110 <stephan.hacker@josephinum.at>
2022-05-18 16:49:12 +0200

    25 fertig

---
+++ 2022_SEW3_stephan.hacker/26TcpLED/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace _26TcpLED
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.nudPort = new System.Windows.Forms.NumericUpDown();
+            this.btnConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(137, 42);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 23);
+            this.txtIP.TabIndex = 0;
+            // 
+            // nudPort
+            // 
+            this.nudPort.Location = new System.Drawing.Point(407, 41);
+            this.nudPort.Name = "nudPort";
+            this.nudPort.Size = new System.Drawing.Size(120, 23);
+            this.nudPort.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(644, 41);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.nudPort);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "TCP LED";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.NumericUpDown nudPort;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
