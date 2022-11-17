690554a6e6b8c198d689766a1c9147d210d1c47e
Stevie1110 <stephan.hacker@josephinum.at>
2022-11-17 09:19:42 +0100

    04, 05 fertig 07 angefangen

---
+++ 2022_SEW3_stephan.hacker/_07Events/Bar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Events
+{
+    internal class Bar
+    {
+        public event EventHandler SpendRoundEvent;
+
+        public string Name { get; set; }
+
+        public void SpendRound()
+        {
+            if(this.SpendRoundEvent != null)            // Wichtig!!!
+            {
+                this.SpendRoundEvent(this, EventArgs.Empty);
+            }
+        }
+    }
+}
