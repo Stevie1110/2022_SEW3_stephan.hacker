139d018ef2f3c3079b7751b2a19b9a45db1ad8a1
Stevie1110 <stephan.hacker@josephinum.at>
2022-11-25 15:30:39 +0100

    .

--- 2022_SEW3_stephan.hacker/13HelloWorldC/Quelle.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-	printf("Hello World\n");		// \n macht einen Zeilenumbruch
-	return 0;						// Wenn das Programm Fehlerfrei durchgelaufen ist, dann 0
-									// Bei einem Fehler irgendein Wert <> 0 --> Fehlercode
-}
--- 2022_SEW3_stephan.hacker/HUE11_2/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HUE11_2
-{
-    public class Class1
-    {
-    }
-}
