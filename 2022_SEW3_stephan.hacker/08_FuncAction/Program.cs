5ea567678160fc0963d631c2ca68b9f0ca139c52
Stevie1110 <stephan.hacker@josephinum.at>
2022-11-25 16:46:05 +0100

    07 fertig, 08, 09, 10 gemacht

---
+++ 2022_SEW3_stephan.hacker/08_FuncAction/Program.cs
@@ -0,0 +1,56 @@
+﻿
+//gleiches Beispiel wie 05Delegates nur mit generischen Datentypen (Func, Action)
+
+static int Addition(int x, int y)
+{
+    return x + y;
+}
+
+static int Substraction(int x, int y)
+{
+    return x - y;
+}
+
+static int Multiplication(int x, int y)
+{
+    return x * y;
+}
+static void PrintWithLeadingSpaces(int x)
+{
+    Console.WriteLine("    " + x);
+}
+
+static void PrintWithLeading0(int x)
+{
+    Console.WriteLine("0000" + x);
+}
+
+
+Func<int, int, int> operation = new Func<int, int, int>(Addition);          //neues generischesFunc-Delegate
+
+int result = operation(4, 5);
+Console.WriteLine("Ergebnis: " + result);
+
+operation = new Func<int, int, int>(Substraction);
+result = operation(4, 5);
+Console.WriteLine("Ergebnis: " + result);
+
+
+operation = Multiplication;             //kurze Sschreibweise einer Zuweisung zu einem delegate
+result = operation(4, 5);
+Console.WriteLine("Ergebnis: " + result);
+
+Action<int> printer = PrintWithLeadingSpaces;
+printer(99);
+Console.WriteLine("---------");
+printer += PrintWithLeading0;       //weitere zusätzliche Methode zuweisen
+printer(66);
+Console.WriteLine("---------");
+printer -= PrintWithLeadingSpaces;
+printer(55);
+printer -= PrintWithLeading0;           //alle Methoden wurden abgemeldet --> printer ist null
+printer(44);
+if (printer != null)
+{
+    printer(44);
+}
---
+++ 2022_SEW3_stephan.hacker/10TimerEventWithLambda/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _10TimerEventWithLambda
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtCounter = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCounter
+            // 
+            this.txtCounter.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtCounter.Location = new System.Drawing.Point(256, 105);
+            this.txtCounter.Name = "txtCounter";
+            this.txtCounter.Size = new System.Drawing.Size(224, 54);
+            this.txtCounter.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(207, 226);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(331, 226);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(98, 36);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(451, 226);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 36);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtCounter);
+            this.Name = "Form1";
+            this.Text = "Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtCounter;
+        private Button btnStart;
+        private Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private Button btnReset;
+    }
+}
