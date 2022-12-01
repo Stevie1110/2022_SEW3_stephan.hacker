42274dfad9333ce2a6a6e557af2aaae9fddc7252
Stevie1110 <stephan.hacker@josephinum.at>
2022-12-01 09:19:50 +0100

    1ter Test

---
+++ 2022_SEW3_stephan.hacker/1.1Test_LastNameChanged/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1Test_LastNameChanged
+{
+    internal class Person
+    {
+        public event EventHandler LastNameChanged;
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void LastNameAway(Person p)
+        {
+            p.LastName -= LastNameChanged;
+            p.LastName -= LastNameChanged;
+            p.LastName -= LastNameChanged;
+        }
+
+        private void LastNameChanged(object? sender, EventArgs e)
+        {
+            Console.WriteLine(this.LastName + " Nachname hat sich geändert");
+        }
+    }
+}
