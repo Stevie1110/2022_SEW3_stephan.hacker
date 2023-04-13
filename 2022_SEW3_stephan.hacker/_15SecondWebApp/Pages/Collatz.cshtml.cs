128d7dda6fbadc11add7277a85d43ad1b8997a64
Stevie1110 <stephan.hacker@josephinum.at>
2023-04-13 09:15:21 +0200

    .

--- 2022_SEW3_stephan.hacker/_15SecondWebApp/Pages/Collatz.cshtml.cs
+++ 2022_SEW3_stephan.hacker/_15SecondWebApp/Pages/Collatz.cshtml.cs
@@ -5,8 +5,34 @@ namespace _15SecondWebApp.Pages
 {
     public class CollatzModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		public void OnGet()
+		{
+		}
+
+		[HttpPost]
+		public IActionResult OnPostCalculateCollatz(int startNum)
+		{
+			string collatz = "There are only natural numbers bigger than 0 allowed";
+
+			if (startNum < 1)
+			{
+				return RedirectToPage("CollatzResult", new { result = collatz, source = "Collatz" });
+			}
+			collatz = startNum.ToString();
+			while (startNum != 1)
+			{
+				collatz += ", ";
+				if (startNum % 2 == 0)
+				{
+					startNum = startNum / 2;
+				}
+				else
+				{
+					startNum = 3 * startNum + 1;
+				}
+				collatz += startNum.ToString();
+			}
+			return RedirectToPage("CollatzResult", new { result = collatz + ", 4, 2, 1, ...", source = "Collatz" });
+		}
+	}
 }
